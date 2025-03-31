d6fac69280403ddbd696ac460302cfa125058f18
Controllerdestiny <523321293@qq.com>
2025-03-31 16:02:12 +0800

    fix log write

--- Lagrange.XocMat/Command/GroupCommands/Wiki.cs
+++ Lagrange.XocMat/Command/GroupCommands/Wiki.cs
@@ -1,4 +1,5 @@
 ﻿using System.Web;
+using Lagrange.Core.Message.Entity;
 using Lagrange.XocMat.Command.CommandArgs;
 using Lagrange.XocMat.Extensions;
 using Lagrange.XocMat.Internal;
@@ -17,5 +18,12 @@ public class Wiki : Command
         string url = "https://terraria.wiki.gg/zh/index.php?search=";
         string msg = args.Parameters.Count > 0 ? url += HttpUtility.UrlEncode(args.Parameters[0]) : url.Split("?")[0];
         await args.Event.Reply(msg);
+    }
+
+    public override async Task InvokeAsync(FriendCommandArgs args, ILogger log)
+    {
+        string url = "https://terraria.wiki.gg/zh/index.php?search=";
+        string msg = args.Parameters.Count > 0 ? url += HttpUtility.UrlEncode(args.Parameters[0]) : url.Split("?")[0];
+        await args.MessageBuilder.Add(new StreamEntity(msg)).Reply();
     }
 }
