f1dbe0239620742e07c08c20df34dea2f3d8ca61
Controllerdestiny <523321293@qq.com>
2025-06-26 05:41:29 +0800

    update

---
+++ Lagrange.XocMat/Command/GroupCommands/ButtonCommand.cs
@@ -0,0 +1,78 @@
+﻿using Lagrange.Core.Message.Entity;
+using Lagrange.XocMat.Command.CommandArgs;
+using Lagrange.XocMat.Extensions;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lagrange.XocMat.Command.GroupCommands;
+
+public class ButtonCommand : Command
+{
+    public override string[] Alias => ["b"];
+
+    public override string[] Permissions => ["button"];
+
+    public override async Task InvokeAsync(GroupCommandArgs args, ILogger log)
+    {
+        var Keyboard = new KeyboardData
+        {
+            Rows =
+            [
+                new()
+                {
+                    Buttons =
+                    [
+                        new()
+                        {
+                            Id = "VERIFICATION_FORUM",
+                            RenderData= new RenderData()
+                            {
+                                Style = 1,
+                                Label = args.Parameters.Count != 0 ? args.Parameters[0] : "Terraria Wiki",
+                            },
+                            Action = new()
+                            {
+                                Type = 0,
+                                Data = $"https://terraria.wiki.gg/zh/index.php?search={(args.Parameters.Count != 0 ? args.Parameters[0] : "")}",
+                                Permission = new()
+                                {
+                                    Type = 2
+                                }
+                            }
+                        },
+                    ]
+                },
+                new()
+                {
+                    Buttons =
+                    [
+                        new()
+                        {
+                            Id = "search",
+                            RenderData= new RenderData()
+                            {
+                                Style = 1,
+                                Label = "再次搜索",
+                            },
+                            Action = new()
+                            {
+                                Type = 2,
+                                Data = $"{args.CommandPrefix}{args.Name}",
+                                Permission = new()
+                                {
+                                    Type = 2
+                                }
+                            }
+                        },
+                    ]
+                }
+            ],
+        };
+        args.MessageBuilder.Keyboard(Keyboard);
+        await args.MessageBuilder.Reply();
+    }
+}
--- Lagrange.XocMat/Command/GroupCommands/Wiki.cs
+++ Lagrange.XocMat/Command/GroupCommands/Wiki.cs
@@ -15,62 +15,9 @@ public class Wiki : Command
 
     public override async Task InvokeAsync(GroupCommandArgs args, ILogger log)
     {
-        var Keyboard = new KeyboardData
-        {
-            Rows =
-            [
-                new()
-                {
-                    Buttons =
-                    [
-                        new()
-                        {
-                            Id = "VERIFICATION_FORUM",
-                            RenderData= new RenderData()
-                            {
-                                Style = 1,
-                                Label = args.Parameters.Count != 0 ? args.Parameters[0] : "Terraria Wiki",
-                            },
-                            Action = new()
-                            {
-                                Type = 0,
-                                Data = $"https://terraria.wiki.gg/zh/index.php?search={(args.Parameters.Count != 0 ? args.Parameters[0] : "")}",
-                                Permission = new()
-                                {
-                                    Type = 2
-                                }
-                            }
-                        },
-                    ]
-                },
-                new()
-                {
-                    Buttons =
-                    [
-                        new()
-                        {
-                            Id = "search",
-                            RenderData= new RenderData()
-                            {
-                                Style = 1,
-                                Label = "再次搜索",
-                            },
-                            Action = new()
-                            {
-                                Type = 2,
-                                Data = $"{args.CommandPrefix}{args.Name}",
-                                Permission = new()
-                                {
-                                    Type = 2
-                                }
-                            }
-                        },
-                    ]
-                }
-            ],
-        };
-        args.MessageBuilder.Keyboard(Keyboard);
-        await args.MessageBuilder.Reply();
+        string url = "https://terraria.wiki.gg/zh/index.php?search=";
+        string msg = args.Parameters.Count > 0 ? url += HttpUtility.UrlEncode(args.Parameters[0]) : url.Split("?")[0];
+        await args.MessageBuilder.Text(msg).Reply();
     }
 
     public override async Task InvokeAsync(FriendCommandArgs args, ILogger log)
